ff2831f42713c7f483ab814ab9d5c55d742cec84
MahbubHasan1994 <mahabubhasan18@gmail.com>
2020-03-09 11:38:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tryyoutube/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.tryyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView mYouTubePlayerView;
+    Button btn;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.okButton);
+        mYouTubePlayerView = findViewById(R.id.youtubeAPI);
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+//                youTubePlayer.loadVideo("W4hTJybfU7s");
+                youTubePlayer.loadPlaylist("PLF-q-IGQQb1sLZqZ9CVlhkMD_9lOTpAeV");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mYouTubePlayerView.initialize(YoutubeConfig.getApiKey(),mOnInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tryyoutube/YoutubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.tryyoutube;
+
+public class YoutubeConfig {
+
+    public YoutubeConfig() {
+    }
+
+    private static final String API_KEY = "AIzaSyCpK5JVfQpAcAZN8ejL135F05kls5Seopc";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
